062512989f4566ee3c72bfd092d07914358ea223
Nayan15 <anunayan123@gmail.com>
2025-05-22 20:26:13 +0530

    Commit2

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,32 @@ import Location from "./Components/Location";
 import Personnel from "./Components/PersonnelDetails";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <>
       <h1 className="text-center text-xl font-bold my-6">POST YOUR AD</h1>
-      <div className="flex justify-center min-h-screen bg-white">
-        <div className="border border-gray-300 rounded-none w-220 p-4">
+      
+      <div className="flex justify-center min-h-screen bg-white px-2">
+        <div className="w-full max-w-3xl border border-gray-300 rounded-none p-4 sm:p-6 bg-white">
           <Category />
-          <div className=" mt-4 border-b border-gray-300 mt-4" />
+          <div className="mt-4 border-b border-gray-300" />
+          
           <Details />
-          <div className="mt-4 border-b border-gray-300 mt-4" />
+          <div className="mt-4 border-b border-gray-300" />
+          
           <Price />
-          <div className="mt-4 border-b border-gray-300 mt-4" />
+          <div className="mt-4 border-b border-gray-300" />
+          
           <Photos />
-          <div className="mt-4 border-b border-gray-300 mt-4" />
+          <div className="mt-4 border-b border-gray-300" />
+          
           <Location />
-          <div className="mt-4 border-b border-gray-300 mt-4" />
+          <div className="mt-4 border-b border-gray-300" />
+          
           <Personnel />
-          <div className="mt-4 border-b border-gray-300 mt-4" />
+          <div className="mt-4 border-b border-gray-300" />
+          
           <button
-            className=" px-5 py-2 mt-2 bg-gray-300 text-gray-600 font-semibold rounded cursor-not-allowed"
+            className="mt-4 px-5 py-2 bg-gray-300 text-gray-600 font-semibold rounded cursor-not-allowed w-fit"
             disabled
           >
             Post Now
